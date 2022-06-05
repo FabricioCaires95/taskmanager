e4a6e26c5938f4eb20209f9f386f6211233e8f8b
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-06-05 19:45:35 -0300

    ajust configs

--- service/src/main/resources/db/migration/V1__create_task_tables.sql
+++ service/src/main/resources/db/migration/V1__create_task_tables.sql
@@ -3,7 +3,8 @@ CREATE TABLE task (
 	title varchar(100) NOT NULL,
 	description varchar(250) NOT NULL,
 	finish_at timestamp with time zone NOT NULL,
-	is_finished boolean NOT NULL,
+	is_finished boolean NOT NULL DEFAULT FALSE,
 	created_at timestamp with time zone NOT NULL,
-	updated_at timestamp with time zone NOT NULL
+	updated_at timestamp with time zone NOT NULL,
+	primary key(id)
 );
