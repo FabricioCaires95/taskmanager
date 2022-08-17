d2fbfd47620a77d7a6ad8bc8fe3664c531992298
Fabricio Caires <fabricio.vieira995@hotmail.com>
2022-08-16 21:07:16 -0300

    implementation update user feature and refactor on getUserById

--- service/src/main/java/br/com/spacer/taskmanager/domain/repository/UserRepository.java
+++ service/src/main/java/br/com/spacer/taskmanager/domain/repository/UserRepository.java
@@ -2,9 +2,17 @@ package br.com.spacer.taskmanager.domain.repository;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import br.com.spacer.taskmanager.domain.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("UPDATE User u SET u.name = :name, u.password = :password WHERE u.id = :uuid")
+    void updateUser(@Param("uuid") UUID id, @Param("name") String name, @Param("password") String password);
+
 }
--- service/src/main/java/br/com/spacer/taskmanager/service/UserService.java
+++ service/src/main/java/br/com/spacer/taskmanager/service/UserService.java
@@ -1,9 +1,14 @@
 package br.com.spacer.taskmanager.service;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.UUID;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import br.com.spacer.taskmanager.api.model.CreateUserDTO;
+import br.com.spacer.taskmanager.api.model.UpdateUserDTO;
 import br.com.spacer.taskmanager.api.model.UserDTO;
+import br.com.spacer.taskmanager.domain.entity.User;
 import br.com.spacer.taskmanager.domain.repository.UserRepository;
 import br.com.spacer.taskmanager.exception.UserNotFoundException;
 import br.com.spacer.taskmanager.mapper.UserMapper;
@@ -29,8 +34,21 @@ public class UserService {
     }
 
     public UserDTO getUserById(UUID id) {
+        var user = getUserOrThrowException(id);
+        return userMapper.fromEntityToDto(user);
+    }
+
+    @Transactional
+    public void updateUser(UUID id, UpdateUserDTO updateUserDTO) {
+        userValidator.validate(updateUserDTO);
+        var userId = getUserOrThrowException(id);
+        userRepository.updateUser(userId.getId(), updateUserDTO.getName(), updateUserDTO.getPassword());
+    }
+
+
+    private User getUserOrThrowException(UUID id) {
+        requireNonNull(id);
         return userRepository.findById(id)
-                .map(userMapper::fromEntityToDto)
                 .orElseThrow(() -> new UserNotFoundException("User not found "));
     }
 
